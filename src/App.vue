66911d1bf2a6ea9aa5fa332374eed5281ed77960
Richard Emijere <emijere.richard@gmail.com>
2023-03-09 08:15:42 +0000

    Added tailwind theme

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-    <p class="text-3xl text-gray-700 font-bold mb-5">Welcome!</p>
-    <p class="text-gray-500 text-lg">Vue and Tailwind CSS in action</p>
+    <p class="text-3xl font-bold mb-5 text-primary">Welcome!</p>
+    <p class="text-secondary text-lg">Vue and Tailwind CSS in action</p>
+    <button class="btn btn-primary">Primary</button>
+    <button class="btn btn-secondary">Primary</button>
+    <button class="btn btn-secondary2">Primary</button>
   </div>
 </template>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      tablet: "960px",
+      desktop: "1248px",
+    },
+    colors: {
+      white: '#FFFFFF',
+      black: '#000000',
+      primary: '#008289',
+      primaryFade: '#60CED4',
+      secondary: '#5271FF',
+      secondary2: '#C34F06',
+      borderColor: '#707070'
+    },
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 };
